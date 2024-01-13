6352c4f83affbd2490076a0a6ffe74a6dbd64f4e
Vdaro <107029324+Vdaro@users.noreply.github.com>
2024-01-13 14:20:59 +0100

    Update main.py ; prevent the chatbot to stop mid-sentence
    This solution seems imperfect and seems to be only working with the test prompt used, which is "Write a short history of France"

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from llama_cpp import Llama
 #
 import streamlit as st
 
+# from transformers import AutoModelForCausalLM, AutoTokenizer
 
-def prompting(_prompt):
+def prompting(model, _prompt):
     # create a text prompt
-    prompt = " Q : " + str(_prompt) + " A : "
+    prompt = "### Q : " + str(_prompt) + " ### A : "
     # generate a response ( takes several seconds )
-    output = LLM(prompt)
+    output = model(prompt, max_tokens=512, stop=["###"])
     # display the response
     #    print(output["choices"][0]["text"])
     st.write(output["choices"][0]["text"])
@@ -21,8 +22,10 @@ prompt = str(st.chat_input("Write your message"))
 # with st.sidebar:
 with st.chat_message("assistant"):
     LLM = Llama(model_path="./llama-2-7b.Q4_K_M.gguf")
-#    LLM.max_new_token = 512
-    prompting(prompt)
+    #    LLM = Llama(model_path="./Mistral-8X7B-v0.1")
+
+    #    LLM.max_new_token = 512
+    prompting(LLM, prompt)
 # st.button('Execute', on_click=prompting(prompt))
 
 
